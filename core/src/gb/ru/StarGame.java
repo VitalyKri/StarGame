73cfc834c2c9b3296db15abe5834b5cb7f08a3ed
Vitaly <vitalykri123@gmail.com>
2021-08-02 21:25:23 +0500

    add lesson2

--- core/src/gb/ru/StarGame.java
+++ core/src/gb/ru/StarGame.java
@@ -1,38 +1,15 @@
 package gb.ru;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-import java.util.Vector;
+import gb.ru.screen.MenuScreen;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	TextureRegion region;
+public class StarGame extends Game {
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("wallpaper.jpg");
-		img.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-		TextureRegion region = new TextureRegion(img,0,0,1080,1920);
 
+		setScreen(new MenuScreen());
 
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
